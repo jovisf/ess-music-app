806f3d4410ab3779da9bd11262faa62f8bef8697
jovisf <jovisf30@gmail.com>
2023-04-16 18:26:45 -0300

    refatorando historico

--- web-app/src/app/historic/historic.component.ts
+++ web-app/src/app/historic/historic.component.ts
@@ -1,57 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
 import { MusicaService } from '../musicas/musicas.service';
 import { Musica } from '../musicas/musica';
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-historic',
   templateUrl: './historic.component.html',
   styleUrls: ['./historic.component.css'],
 })
-export class HistoricComponent {
+export class HistoricComponent implements OnInit {
   
   user: any;
   historico: number[] = [];
   musicas: Musica[] = [];
   musicasHistorico: Musica[] = [];
 
-
-  constructor(private service: AuthService, private dialog: MatDialog, private http:HttpClient, private musicaService: MusicaService) {
-
-    
-    
-    this.musicaService.getMusicas().subscribe(res => {
-      this.musicas = res;
-    })
-    
-  }
-
-
-
-
-
-  limparHistorico(){ // limpa o historico de musicas
-    this.http.patch(`http://localhost:3000/user/${this.user.id}/`, {"historico" : []}).subscribe();
-    window.location.reload();
-  }
+  constructor(
+    private authService: AuthService,
+    private dialog: MatDialog,
+    private http: HttpClient,
+    private musicaService: MusicaService
+  ) {}
 
   ngOnInit(): void {
-    this.user = this.service.GetbyCode(localStorage.getItem('username')).subscribe(res => {
-      this.user = res;
-      this.historico = this.user.historico;
-    // compara os ids nos historicos com todas as musicas no banco
-    for (let index = 0; index < this.musicas.length; index++) { 
-      for (let indexhistorico = 0; indexhistorico < this.historico.length; indexhistorico++) {
-        if (this.historico[indexhistorico] == this.musicas[index].id) {
-            this.musicasHistorico = [...this.musicasHistorico, this.musicas[index]] 
-        } 
-      }   
-    }
+    this.initComponent();
+  }
+
+  private initComponent(): void {
+    this.musicaService.getMusicas().subscribe(res => {
+      this.musicas = res;
+    });
+
+    this.authService.GetbyCode(localStorage.getItem('username')).pipe(
+      tap(res => {
+        this.user = res;
+        this.historico = this.user.historico;
+      }),
+      catchError(err => {
+        console.error('Erro ao obter usuário:', err);
+        return of(null);
+      })
+    ).subscribe(() => {
+      this.filtrarMusicasHistorico();
+    });
+  }
+
+  private filtrarMusicasHistorico(): void {
+    this.musicasHistorico = this.musicas.filter(musica => this.historico.includes(musica.id)).reverse();
+  }
+
+  limparHistorico(): void {
+    this.http.patch(`http://localhost:3000/user/${this.user.id}/`, {"historico" : []}).subscribe(() => {
+      window.location.reload();
     });
   }
 }
